aca7f044b87f852cacca4e29cd5ac7b01fe1787d
Hassan Jasim <hassanjasim1000@gmail.com>
2018-09-27 14:50:20 -0400

    Add files via upload
    Calculates the magnitude and angle of a vector given the X and Y components

---
+++ HassanJasim_Lab_2.1/HassanJasim_Lab_2.1/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtXComp = New System.Windows.Forms.TextBox()
+        Me.txtYComp = New System.Windows.Forms.TextBox()
+        Me.chkAngle = New System.Windows.Forms.CheckBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "X-Component (N)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Y-Component (N)"
+        '
+        'txtXComp
+        '
+        Me.txtXComp.Location = New System.Drawing.Point(164, 38)
+        Me.txtXComp.Name = "txtXComp"
+        Me.txtXComp.Size = New System.Drawing.Size(221, 22)
+        Me.txtXComp.TabIndex = 2
+        '
+        'txtYComp
+        '
+        Me.txtYComp.Location = New System.Drawing.Point(164, 72)
+        Me.txtYComp.Name = "txtYComp"
+        Me.txtYComp.Size = New System.Drawing.Size(221, 22)
+        Me.txtYComp.TabIndex = 3
+        '
+        'chkAngle
+        '
+        Me.chkAngle.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.chkAngle.Location = New System.Drawing.Point(107, 110)
+        Me.chkAngle.Name = "chkAngle"
+        Me.chkAngle.Size = New System.Drawing.Size(208, 26)
+        Me.chkAngle.TabIndex = 4
+        Me.chkAngle.Text = "Display Intermediate angle"
+        Me.chkAngle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.chkAngle.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(35, 142)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(350, 23)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "&Evaluate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(32, 182)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(353, 273)
+        Me.lblOutput.TabIndex = 6
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(35, 468)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(174, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(211, 468)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(174, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(420, 532)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.chkAngle)
+        Me.Controls.Add(Me.txtYComp)
+        Me.Controls.Add(Me.txtXComp)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtXComp As System.Windows.Forms.TextBox
+    Friend WithEvents txtYComp As System.Windows.Forms.TextBox
+    Friend WithEvents chkAngle As System.Windows.Forms.CheckBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ HassanJasim_Lab_2.1/HassanJasim_Lab_2.1/Form1.vb
@@ -0,0 +1,80 @@
+﻿'Hassan Jasim 400057926
+Option Strict On
+Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim xComp, yComp, resultant, angle, finalAngle As Double
+
+
+        Try
+
+
+            If Not Double.TryParse(txtXComp.Text, xComp) Then
+                MsgBox("The X component is invalid", , "ERROR")
+                txtXComp.Focus()
+                Exit Sub
+            End If
+            If Not Double.TryParse(txtYComp.Text, yComp) Then
+                MsgBox("The Y component is invalid", , "ERROR")
+                txtYComp.Focus()
+                Exit Sub
+            End If
+
+
+            xComp = CDbl(txtXComp.Text)
+            yComp = CDbl(txtYComp.Text)
+
+
+
+            If xComp <> 0 And yComp <> 0 Then
+
+                resultant = Math.Sqrt((xComp ^ 2) + (yComp ^ 2))
+                angle = Math.Atan(yComp / xComp)
+
+
+                If xComp < 0 And yComp > 0 Then
+                    finalAngle = Math.PI + angle
+
+                ElseIf xComp < 0 And yComp < 0 Then
+                    finalAngle = Math.PI + angle
+
+                ElseIf xComp > 0 And yComp < 0 Then
+                    finalAngle = (2 * Math.PI) + angle
+
+                Else
+                    finalAngle = angle
+                End If
+
+                angle = angle * (180 / Math.PI)
+                finalAngle = finalAngle * (180 / Math.PI)
+                lblOutput.Text = "The X component is " & xComp & "N" & vbCr
+                lblOutput.Text &= "The Y component is " & yComp & "N" & vbCr
+                lblOutput.Text &= "The Resultant is " & resultant.ToString("n1") & "N" & vbCr
+                lblOutput.Text &= "The Final Angle is " & finalAngle.ToString("n1") & " Degrees" & vbCr
+                If chkAngle.Checked Then
+                    lblOutput.Text &= "The Intermediate Angle is " & angle.ToString("n1") & " Degrees" & vbCr
+                End If
+
+            Else
+                MessageBox.Show("The X & Y component can't be ZERO", "ERROR")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error")
+        End Try
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtXComp.Clear()
+        txtYComp.Clear()
+        lblOutput.Text = String.Empty
+        chkAngle.Checked = False
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
