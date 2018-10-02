b07e36f18f29daa994d695ac1330f5b8198cad2f
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-02 15:01:51 -0400

    Calculates total price of an item and its tax

---
+++ HassanJasim_Lab_3.1/HassanJasim_Lab_3.1/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.radRed = New System.Windows.Forms.RadioButton()
+        Me.radYellow = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.chkTax = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Quantity"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = " Price"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(251, 52)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(269, 22)
+        Me.txtQuantity.TabIndex = 2
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(251, 85)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(269, 22)
+        Me.txtPrice.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(31, 199)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(489, 23)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add Item"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 21)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(251, 21)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(269, 22)
+        Me.txtName.TabIndex = 6
+        '
+        'radRed
+        '
+        Me.radRed.AutoSize = True
+        Me.radRed.Location = New System.Drawing.Point(165, 21)
+        Me.radRed.Name = "radRed"
+        Me.radRed.Size = New System.Drawing.Size(55, 21)
+        Me.radRed.TabIndex = 7
+        Me.radRed.TabStop = True
+        Me.radRed.Text = "Red"
+        Me.radRed.UseVisualStyleBackColor = True
+        '
+        'radYellow
+        '
+        Me.radYellow.AutoSize = True
+        Me.radYellow.Location = New System.Drawing.Point(340, 21)
+        Me.radYellow.Name = "radYellow"
+        Me.radYellow.Size = New System.Drawing.Size(69, 21)
+        Me.radYellow.TabIndex = 8
+        Me.radYellow.TabStop = True
+        Me.radYellow.Text = "Yellow"
+        Me.radYellow.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radRed)
+        Me.GroupBox1.Controls.Add(Me.radYellow)
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 113)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(489, 53)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Item Type"
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 16
+        Me.lstOutput.Location = New System.Drawing.Point(31, 228)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(489, 260)
+        Me.lstOutput.TabIndex = 10
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(31, 494)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(239, 23)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(281, 494)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(239, 23)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'chkTax
+        '
+        Me.chkTax.AutoSize = True
+        Me.chkTax.Location = New System.Drawing.Point(222, 172)
+        Me.chkTax.Name = "chkTax"
+        Me.chkTax.Size = New System.Drawing.Size(118, 21)
+        Me.chkTax.TabIndex = 13
+        Me.chkTax.Text = "Display Taxes"
+        Me.chkTax.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 535)
+        Me.Controls.Add(Me.chkTax)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents radRed As RadioButton
+    Friend WithEvents radYellow As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents chkTax As CheckBox
+End Class
---
+++ HassanJasim_Lab_3.1/HassanJasim_Lab_3.1/Form1.vb
@@ -0,0 +1,115 @@
+﻿'Hassan Jasim 400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim itemQuantity As Integer
+        Dim itemPrice, Subtotal, HST, totalPrice As Double
+        Dim itemName As String = String.Empty
+
+        If txtName.Text = String.Empty Then
+            txtName.Clear()
+            txtName.Focus()
+            MsgBox("Please Enter a valid item name",, "ERROR")
+            Exit Sub
+        End If
+
+        itemName = txtName.Text
+        itemName = itemName.Trim()
+
+        If Not Integer.TryParse(txtQuantity.Text, itemQuantity) Then
+            txtQuantity.Clear()
+            txtQuantity.Focus()
+            MsgBox("Please Enter a valid Quantity",, "ERROR")
+            Exit Sub
+        End If
+
+        If Not Double.TryParse(txtPrice.Text, itemPrice) Then
+            txtPrice.Clear()
+            txtPrice.Focus()
+            MessageBox.Show("Item Price cannot be alphabetical!")
+            Exit Sub
+        End If
+
+        If itemQuantity <= 0 Then
+            MessageBox.Show("Item Quantity cannot be less than or equal to Zero")
+            Exit Sub
+        End If
+
+        If itemPrice <= 0 Then
+            MessageBox.Show("Item Price cannot be less than or equal to Zero")
+            Exit Sub
+        End If
+
+        Subtotal = price(itemPrice, itemQuantity)
+
+        If chkTax.Checked Then
+            HST = Subtotal * 0.13
+            totalPrice = HST + Subtotal
+            lstOutput.Items.Add("Item's Name: " & itemName.ToString() & vbCrLf)
+            lstOutput.Items.Add("Item's Quantity: " & itemQuantity.ToString() & vbCrLf)
+            lstOutput.Items.Add("Item's Price: " & itemPrice.ToString("c") & vbCrLf)
+            lstOutput.Items.Add("Subtotal: " & Subtotal.ToString("c") & vbCrLf)
+            lstOutput.Items.Add("HST: " & HST.ToString("c") & vbCrLf)
+            lstOutput.Items.Add("Total Amount: " & totalPrice.ToString("c"))
+        Else
+            lstOutput.Items.Add("Item's Name: " & itemName.ToString() & vbCrLf)
+            lstOutput.Items.Add("Item's Quantity: " & itemQuantity.ToString() & vbCrLf)
+            lstOutput.Items.Add("Item's Price: " & itemPrice.ToString("c") & vbCrLf)
+            lstOutput.Items.Add("Subtotal: " & Subtotal.ToString("c") & vbCrLf)
+        End If
+
+        txtName.Text = String.Empty
+        txtPrice.Text = String.Empty
+        txtQuantity.Text = String.Empty
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtName.Clear()
+        txtPrice.Clear()
+        txtQuantity.Clear()
+        lstOutput.Items.Clear()
+        radRed.Checked = False
+        radYellow.Checked = False
+        chkTax.Checked = False
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    Private Function price(ByVal itemPrice As Double, ByVal itemQuantity As Integer) As Double
+
+        Dim Subtotal As Double
+        Dim Discount As Double
+
+        If radRed.Checked Then
+            Subtotal = itemPrice * itemQuantity
+
+            If itemQuantity = 1 Then
+                Discount = Subtotal * 0.2
+                Subtotal -= Discount
+
+            ElseIf itemQuantity = 2 Then
+                Discount = Subtotal * 0.3
+                Subtotal -= Discount
+
+            ElseIf itemQuantity > 2 Then
+                Discount = Subtotal * 0.4
+                Subtotal -= Discount
+            End If
+        End If
+
+        If radYellow.Checked Then
+            Subtotal = itemPrice * itemQuantity
+            If itemQuantity = 2 Then
+                Discount = Subtotal * 0.1
+                Subtotal -= Discount
+
+            ElseIf itemQuantity > 2 Then
+                Discount = Subtotal * 0.15
+                Subtotal -= Discount
+            End If
+        End If
+
+        Return Subtotal
+    End Function
+End Class
