3bfa60c400154aa13be0784c9ce77a097da047e5
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-06 23:05:33 -0400

    Add files via upload

---
+++ HassanJasim_Lab_3.2/HassanJasim_Lab_3.2/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMin = New System.Windows.Forms.TextBox()
+        Me.txtMax = New System.Windows.Forms.TextBox()
+        Me.btnFindPrimeNum = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(177, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the Minimum number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter the Maximum number"
+        '
+        'txtMin
+        '
+        Me.txtMin.Location = New System.Drawing.Point(247, 32)
+        Me.txtMin.Name = "txtMin"
+        Me.txtMin.Size = New System.Drawing.Size(194, 22)
+        Me.txtMin.TabIndex = 2
+        '
+        'txtMax
+        '
+        Me.txtMax.Location = New System.Drawing.Point(247, 62)
+        Me.txtMax.Name = "txtMax"
+        Me.txtMax.Size = New System.Drawing.Size(194, 22)
+        Me.txtMax.TabIndex = 3
+        '
+        'btnFindPrimeNum
+        '
+        Me.btnFindPrimeNum.Location = New System.Drawing.Point(31, 117)
+        Me.btnFindPrimeNum.Name = "btnFindPrimeNum"
+        Me.btnFindPrimeNum.Size = New System.Drawing.Size(410, 23)
+        Me.btnFindPrimeNum.TabIndex = 4
+        Me.btnFindPrimeNum.Text = "Find All Prime Numbers"
+        Me.btnFindPrimeNum.UseVisualStyleBackColor = True
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 16
+        Me.lstOutput.Location = New System.Drawing.Point(31, 146)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(410, 180)
+        Me.lstOutput.TabIndex = 5
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(31, 332)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(201, 23)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(240, 332)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(201, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 405)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Controls.Add(Me.btnFindPrimeNum)
+        Me.Controls.Add(Me.txtMax)
+        Me.Controls.Add(Me.txtMin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMin As TextBox
+    Friend WithEvents txtMax As TextBox
+    Friend WithEvents btnFindPrimeNum As Button
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ HassanJasim_Lab_3.2/HassanJasim_Lab_3.2/Form1.vb
@@ -0,0 +1,51 @@
+﻿Option Strict On
+Public Class Form1
+    Private Sub btnFindPrimeNum_Click(sender As Object, e As EventArgs) Handles btnFindPrimeNum.Click
+        Dim n, m As Integer
+        If Not Integer.TryParse(txtMin.Text, m) Then
+            txtMin.Clear()
+            txtMin.Focus()
+            MsgBox("Please enter a valid Minimum",, "ERROR")
+            Exit Sub
+        End If
+        If Not Integer.TryParse(txtMax.Text, n) Then
+            txtMax.Clear()
+            txtMax.Focus()
+            MsgBox("Please enter a valid Maximum",, "ERROR")
+            Exit Sub
+        End If
+        If isInputValid(m, n) Then
+            For i = m To n
+                If isPrime(i) Then
+                    lstOutput.Items.Add(i)
+                End If
+            Next
+        End If
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        resetControls()
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    Private Sub resetControls()
+        lstOutput.Items.Clear()
+        txtMax.Clear()
+        txtMin.Clear()
+    End Sub
+    Private Function isInputValid(ByVal m As Integer, ByVal n As Integer) As Boolean
+        If m > 1 And m < n Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Private Function isPrime(ByVal i As Integer) As Boolean
+        For x = 2 To i / 2
+            If i Mod x = 0 Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+End Class
