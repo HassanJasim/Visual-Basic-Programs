500bdce3d671d617787a2613c3901fd3d5986e61
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-08 18:38:29 -0400

    Add files via upload

---
+++ HassanJasim_Lab_4.1/HassanJasim_Lab_4.1/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnTriangles = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnTriangles
+        '
+        Me.btnTriangles.Location = New System.Drawing.Point(24, 23)
+        Me.btnTriangles.Name = "btnTriangles"
+        Me.btnTriangles.Size = New System.Drawing.Size(446, 26)
+        Me.btnTriangles.TabIndex = 0
+        Me.btnTriangles.Text = "Triangles"
+        Me.btnTriangles.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(24, 284)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(219, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(251, 284)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(219, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(24, 56)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(446, 225)
+        Me.lblOutput.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 328)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnTriangles)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnTriangles As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ HassanJasim_Lab_4.1/HassanJasim_Lab_4.1/Form1.vb
@@ -0,0 +1,52 @@
+﻿'Hassan Jasim 
+'ID:400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnTriangles_Click(sender As Object, e As EventArgs) Handles btnTriangles.Click
+        Dim ArrayTriangle(4, 2) As Integer
+        Dim Perimeter(4, 1) As Integer
+        Dim Area(4, 1) As Double
+
+        getTriangleSides(ArrayTriangle)
+        getProperties(ArrayTriangle, Perimeter, Area)
+        lblOutput.TextAlign = ContentAlignment.TopLeft
+
+        lblOutput.Text = "Triangle:    Side a:     Side b:     Side c:     Perimeter:     Area:" & vbCrLf
+
+        For i As Integer = 0 To 4
+            lblOutput.Text &= (i + 1).ToString & "                 " & ArrayTriangle(i, 0).ToString("f1") & "           " & ArrayTriangle(i, 1).ToString("n1") & "           " & ArrayTriangle(i, 2).ToString("n1") & "           " & Perimeter(i, 1).ToString("n1") & "            " & Area(i, 1).ToString("n1") & vbCrLf
+        Next
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblOutput.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub getTriangleSides(ByVal ArrayTriangle(,) As Integer)
+        Dim rand As New Random
+
+        For i As Integer = 0 To 4
+            Do Until (ArrayTriangle(i, 0) + ArrayTriangle(i, 1)) > ArrayTriangle(i, 2) And (ArrayTriangle(i, 1) + ArrayTriangle(i, 2)) > ArrayTriangle(i, 0) And (ArrayTriangle(i, 0) + ArrayTriangle(i, 2)) > ArrayTriangle(i, 1)
+                For j As Integer = 0 To 2
+                    ArrayTriangle(i, j) = rand.Next(9) + 1
+                Next
+            Loop
+        Next
+    End Sub
+
+    Private Sub getProperties(ByVal ArrayTriangle(,) As Integer, ByVal Perimeter(,) As Integer, ByVal Area(,) As Double)
+        Dim p(4) As Double
+
+        For i As Integer = 0 To 4
+            For j As Integer = 0 To 2
+                Perimeter(i, 1) = ArrayTriangle(i, 0) + ArrayTriangle(i, 1) + ArrayTriangle(i, 2)
+                p(i) = Perimeter(i, 1) / 2
+                Area(i, 1) = Math.Sqrt(p(i) * (p(i) - ArrayTriangle(i, 0)) * (p(i) - ArrayTriangle(i, 1)) * (p(i) - ArrayTriangle(i, 2)))
+            Next
+        Next
+    End Sub
+End Class
