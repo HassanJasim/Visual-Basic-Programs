38f3e36d40b5c4ecbfb19ef30f49287a7ea4be9d
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-10 17:25:54 -0400

    Add files via upload

---
+++ HassanJasim_Lab_5.1/HassanJasim_Lab_5.1/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnValidate = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(38, 51)
+        Me.txtInput.MaxLength = 12
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(422, 22)
+        Me.txtInput.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(133, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(239, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Please enter the product code below"
+        '
+        'btnValidate
+        '
+        Me.btnValidate.Location = New System.Drawing.Point(38, 89)
+        Me.btnValidate.Name = "btnValidate"
+        Me.btnValidate.Size = New System.Drawing.Size(422, 23)
+        Me.btnValidate.TabIndex = 2
+        Me.btnValidate.Text = "Validate"
+        Me.btnValidate.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.Control
+        Me.lblOutput.Location = New System.Drawing.Point(38, 126)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(422, 168)
+        Me.lblOutput.TabIndex = 3
+        Me.lblOutput.Text = "At least 7 characters, and no more than 12 characters" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "The first character must b" &
+    "e A, M or P, either upper case or lower case" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "All other characters must be numer" &
+    "ic (0 through 9)"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(38, 297)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(204, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(256, 297)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(204, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 335)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnValidate)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnValidate As Button
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ HassanJasim_Lab_5.1/HassanJasim_Lab_5.1/Form1.vb
@@ -0,0 +1,49 @@
+﻿'HassanJasim
+'400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnValidate_Click(sender As Object, e As EventArgs) Handles btnValidate.Click
+        Dim strInput As String
+        lblOutput.Text = String.Empty
+
+        strInput = txtInput.Text
+
+        If ValidateCode(strInput) Then
+            lblOutput.Text &= "Your Code is Valid!!!"
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtInput.Clear()
+        lblOutput.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Function ValidateCode(ByVal strInput As String) As Boolean
+        Dim charOne As String
+        Dim counter As Integer
+
+        charOne = CStr(strInput.Substring(0, 1))
+
+        If charOne.ToUpper() <> "A" And charOne.ToUpper() <> "M" And charOne.ToUpper() <> "P" Then
+            lblOutput.Text &= "Your code must start with either ""A"", ""M"", ""P""" & vbCr
+            counter += 1
+        End If
+        If strInput.Length - 1 < 7 Then
+            lblOutput.Text &= "The code must be between 7 and 12 characters" & vbCr
+            counter += 1
+        End If
+        For i = 1 To strInput.Length - 1
+            If Not IsNumeric(strInput(i)) Then
+                lblOutput.Text &= "Character " & i + 1 & " must be a number" & vbCr
+                counter += 1
+            End If
+        Next
+
+        Return counter = 0
+    End Function
+End Class
