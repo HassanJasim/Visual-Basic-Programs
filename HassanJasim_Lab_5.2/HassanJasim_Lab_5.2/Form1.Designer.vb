d9b2398495b9e181122e308c2cd70af4f2a003df
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-11 17:43:10 -0400

    Add files via upload

---
+++ HassanJasim_Lab_5.2/HassanJasim_Lab_5.2/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnMakeCall = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(39, 47)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(466, 22)
+        Me.txtPhoneNumber.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(172, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(216, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Please enter your phone number"
+        '
+        'btnMakeCall
+        '
+        Me.btnMakeCall.Location = New System.Drawing.Point(39, 76)
+        Me.btnMakeCall.Name = "btnMakeCall"
+        Me.btnMakeCall.Size = New System.Drawing.Size(466, 23)
+        Me.btnMakeCall.TabIndex = 2
+        Me.btnMakeCall.Text = "Call Me"
+        Me.btnMakeCall.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(39, 289)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(229, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(274, 289)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(231, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.Location = New System.Drawing.Point(39, 106)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(466, 168)
+        Me.lblOutput.TabIndex = 5
+        Me.lblOutput.Text = "                       Enter your Number in the following format" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "               " &
+    "                               (xxx) xxx-xxxx"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(538, 327)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnMakeCall)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPhoneNumber As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnMakeCall As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ HassanJasim_Lab_5.2/HassanJasim_Lab_5.2/Form1.vb
@@ -0,0 +1,76 @@
+﻿'HassanJasim
+'400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnMakeCall_Click(sender As Object, e As EventArgs) Handles btnMakeCall.Click
+        Dim strInput As String
+        lblOutput.Text = String.Empty
+
+        strInput = txtPhoneNumber.Text
+
+        If ValidateNumber(strInput) Then
+            lblOutput.Text &= "Your Number is valid"
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblOutput.Text = "                       Enter your Number in the following format
+                                              (xxx) xxx-xxxx"
+        txtPhoneNumber.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Function ValidateNumber(ByVal strInput As String) As Boolean
+        Dim errorCounter As Integer
+
+        For i = 0 To strInput.Length - 1
+            If i = 0 Then
+                If Not strInput(i) = "(" Then
+                    lblOutput.Text &= "The first charater must be ""(""" & vbCr
+                    errorCounter += 1
+                End If
+            End If
+            If i = 1 Then
+                If Not Convert.ToInt16(strInput(i)) - 48 > 0 Then
+                    lblOutput.Text &= "The first number must be greater than 0" & vbCr
+                    errorCounter += 1
+                End If
+            End If
+
+            If i = 4 Then
+                If Not strInput(i) = ")" Then
+                    lblOutput.Text &= "The Fifth charater must be "")""" & vbCr
+                    errorCounter += 1
+                End If
+            End If
+
+            If i = 5 Then
+                If Not strInput(i) = " " Then
+                    lblOutput.Text &= "The sixth charater must be "" """ & vbCr
+                    errorCounter += 1
+                End If
+            End If
+
+            If i = 9 Then
+                If Not strInput(i) = "-" Then
+                    lblOutput.Text &= "The tenth charater must be ""-""" & vbCr
+                    errorCounter += 1
+                End If
+            End If
+
+            If i <> 0 And i <> 1 And i <> 4 And i <> 5 And i <> 9 Then
+                If Not IsNumeric(strInput(i)) Then
+                    lblOutput.Text &= "The " & i + 1 & " charater must be a number " & vbCr
+                    errorCounter += 1
+                End If
+            End If
+        Next
+
+        Return errorCounter = 0
+
+    End Function
+End Class
