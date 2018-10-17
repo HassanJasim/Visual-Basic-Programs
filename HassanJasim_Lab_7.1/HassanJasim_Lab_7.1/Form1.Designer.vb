6138cb6b65a6943cfd0091c7fbcafb62d3a3023e
Hassan Jasim <hassanjasim1000@gmail.com>
2018-10-17 06:07:14 -0400

    Add files via upload

---
+++ HassanJasim_Lab_7.1/HassanJasim_Lab_7.1/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lstYoungM = New System.Windows.Forms.ListBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtChange = New System.Windows.Forms.TextBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(510, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 45)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Values of Young's Modulus" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "of solid materials (10^9 is " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "adjusted in the formula)" &
+    ""
+        '
+        'lstYoungM
+        '
+        Me.lstYoungM.FormattingEnabled = True
+        Me.lstYoungM.ItemHeight = 16
+        Me.lstYoungM.Location = New System.Drawing.Point(510, 64)
+        Me.lstYoungM.Name = "lstYoungM"
+        Me.lstYoungM.Size = New System.Drawing.Size(155, 100)
+        Me.lstYoungM.TabIndex = 24
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(15, 185)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(650, 44)
+        Me.lblOutput.TabIndex = 23
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(383, 13)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(121, 43)
+        Me.btnCalculate.TabIndex = 22
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtChange
+        '
+        Me.txtChange.Location = New System.Drawing.Point(233, 134)
+        Me.txtChange.Name = "txtChange"
+        Me.txtChange.Size = New System.Drawing.Size(117, 22)
+        Me.txtChange.TabIndex = 21
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(233, 78)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(117, 22)
+        Me.txtLength.TabIndex = 20
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(175, 34)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Enter the change in length" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "0m < L < 1.5X10^-3m"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(145, 34)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Enter the initial length" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "10m < Lo < 20m"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 34)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Enter the Cross-sectional area" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "0.01 < A < 0.2 (m^2)"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(383, 121)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(121, 43)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(383, 67)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(121, 43)
+        Me.btnClear.TabIndex = 15
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'txtArea
+        '
+        Me.txtArea.Location = New System.Drawing.Point(233, 22)
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.Size = New System.Drawing.Size(117, 22)
+        Me.txtArea.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(700, 250)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lstYoungM)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtChange)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtArea)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lstYoungM As ListBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtChange As TextBox
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents txtArea As TextBox
+End Class
---
+++ HassanJasim_Lab_7.1/HassanJasim_Lab_7.1/Form1.vb
@@ -0,0 +1,118 @@
+﻿'Hassan Jasim
+'ID: 400057926
+Option Strict On
+Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim inputFile As StreamReader
+        Dim tempstr As String
+        Dim yModulusArray(6) As Double
+        Dim j As Integer = 0
+        If File.Exists("YM.txt") Then
+            inputFile = File.OpenText("YM.txt")
+            Do Until inputFile.EndOfStream
+
+                tempstr = inputFile.ReadLine()
+                lstYoungM.Items.Add(tempstr)
+
+
+                For i = 0 To tempstr.Length - 1 Step 1
+                    If IsNumeric(tempstr(i)) And j < yModulusArray.Length - 1 Then
+                        yModulusArray(j) = CDbl(tempstr.Substring(i))
+                        j += 1
+                    End If
+                Next
+
+            Loop
+            inputFile.Close()
+        Else
+            MsgBox("The file does not exist!!",, "Error")
+        End If
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblArea, dblLength, dblChange, dblForce, dblYoung As Double
+        Dim counter As Integer
+        Dim tempStr, materialName As String
+
+        tempStr = CStr(lstYoungM.SelectedItem)
+        materialName = String.Empty
+
+        For i = 0 To tempStr.Length - 1
+            If IsNumeric(tempStr(i)) Then
+                tempStr = tempStr.Substring(i)
+                Exit For
+            End If
+            If tempStr(i) = "," Then
+                materialName = tempStr.Substring(0, i - counter)
+                counter += 1
+            End If
+        Next
+
+        dblYoung = CDbl(tempStr)
+
+        If Not Double.TryParse(txtArea.Text, dblArea) Then
+            txtArea.Clear()
+            txtArea.Focus()
+            MsgBox("Please enter a valid Area",, "Error")
+            Exit Sub
+        End If
+        If Not Double.TryParse(txtLength.Text, dblLength) Then
+            txtLength.Clear()
+            txtLength.Focus()
+            MsgBox("Please enter a valid Length",, "Error")
+            Exit Sub
+        End If
+        If Not Double.TryParse(txtChange.Text, dblChange) Then
+            txtChange.Clear()
+            txtChange.Focus()
+            MsgBox("Please enter a valid Change in length",, "Error")
+            Exit Sub
+        End If
+
+        Try
+            dblArea = CDbl(txtArea.Text)
+            dblChange = CDbl(txtChange.Text)
+            dblLength = CDbl(txtLength.Text)
+
+            If dblArea >= 0.01 And dblArea <= 0.2 Then
+                If dblLength >= 10 And dblLength <= 20 Then
+                    If dblChange > 0 And dblChange <= 0.0015 Then
+                        dblForce = (dblChange / dblLength) * dblArea * (dblYoung * 10 ^ 9)
+                        lblOutput.Text = "The forcce needed to change a " & dblLength & "m " & " rod of " & materialName & " by " & dblChange & "m, is " & dblForce & "N, if it has a cross-sectional area of " & dblArea & "m^2"
+                    Else
+                        txtChange.Clear()
+                        txtChange.Focus()
+                        MsgBox("Please enter a Change in length within the given range",, "Error")
+                    End If
+                Else
+                    txtLength.Clear()
+                    txtLength.Focus()
+                    MsgBox("Please enter a Length within the given range",, "Error")
+                End If
+            Else
+                txtArea.Clear()
+                txtArea.Focus()
+                MsgBox("Please enter an Area within the given range",, "Error")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "ERROR")
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtArea.Clear()
+        txtLength.Clear()
+        txtChange.Clear()
+        lblOutput.Text = String.Empty
+        lstYoungM.SelectedIndex = -1
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
