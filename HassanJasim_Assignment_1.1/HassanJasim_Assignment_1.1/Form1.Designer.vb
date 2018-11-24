35b413044dfcc5c8944324c6a9deb028aa304cdc
Hassan Jasim <hassanjasim1000@gmail.com>
2018-11-24 15:27:46 -0500

    Add files via upload

---
+++ HassanJasim_Assignment_1.1/HassanJasim_Assignment_1.1/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSeconds = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtSeconds
+        '
+        Me.txtSeconds.Location = New System.Drawing.Point(273, 28)
+        Me.txtSeconds.Name = "txtSeconds"
+        Me.txtSeconds.Size = New System.Drawing.Size(164, 22)
+        Me.txtSeconds.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter the Number of Seconds"
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(39, 88)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(398, 216)
+        Me.lblOutput.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(39, 62)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(398, 23)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(39, 319)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(199, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(238, 319)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(199, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 355)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSeconds)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSeconds As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ HassanJasim_Assignment_1.1/HassanJasim_Assignment_1.1/Form1.vb
@@ -0,0 +1,70 @@
+﻿'Hassan Jasim 400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Const minSec As Integer = 60
+        Const hourSec As Integer = 3600
+        Const daySec As Integer = 86400
+        Dim days, hours, minutes, seconds As Integer
+        Dim inputSec As Integer
+
+
+        If Not Integer.TryParse(txtSeconds.Text, inputSec) Then
+            txtSeconds.Clear()
+            txtSeconds.Focus()
+            MsgBox("Please enter a valid number of seconds", , "Error")
+            Exit Sub
+        End If
+
+        inputSec = CInt(txtSeconds.Text)
+
+        If inputSec < 0 Then
+            MsgBox("The number of seconds can't be negative", , "Error")
+            txtSeconds.Clear()
+            txtSeconds.Focus()
+            Exit Sub
+        End If
+
+        Try
+            days = CInt(Math.Floor(inputSec / daySec))
+
+            If days >= 1 Then
+                inputSec = inputSec - daySec * days
+            End If
+
+            hours = CInt(Math.Floor(inputSec / hourSec))
+
+            If hours >= 1 Then
+                inputSec = inputSec - hourSec * hours
+            End If
+
+            minutes = CInt(Math.Floor(inputSec / minSec))
+
+            If minutes >= 1 Then
+                inputSec = inputSec - minSec * minutes
+            End If
+
+            seconds = inputSec
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "ERROR")
+        End Try
+
+
+
+        lblOutput.Text = "Days: " & days & vbCr
+        lblOutput.Text &= "Hours: " & hours & vbCr
+        lblOutput.Text &= "Minutes: " & minutes & vbCr
+        lblOutput.Text &= "Seconds: " & seconds & vbCr
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtSeconds.Clear()
+        lblOutput.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
