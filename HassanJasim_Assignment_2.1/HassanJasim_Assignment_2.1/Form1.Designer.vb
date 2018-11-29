d6884ccfc6c4295815d381222c2373e99adbd910
Hassan Jasim <hassanjasim1000@gmail.com>
2018-11-29 14:31:11 -0500

    Add files via upload

---
+++ HassanJasim_Assignment_2.1/HassanJasim_Assignment_2.1/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtP0 = New System.Windows.Forms.TextBox()
+        Me.txtK = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtP0
+        '
+        Me.txtP0.Location = New System.Drawing.Point(264, 58)
+        Me.txtP0.Name = "txtP0"
+        Me.txtP0.Size = New System.Drawing.Size(188, 22)
+        Me.txtP0.TabIndex = 0
+        '
+        'txtK
+        '
+        Me.txtK.Location = New System.Drawing.Point(264, 116)
+        Me.txtK.Name = "txtK"
+        Me.txtK.Size = New System.Drawing.Size(188, 22)
+        Me.txtK.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 34)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter the value of P0 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(P0 > 0) "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(137, 34)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter the value of K " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(-0.08 <= K <= 0.08)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(159, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Growth and Decay"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(44, 161)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(408, 26)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(44, 190)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(408, 362)
+        Me.lblOutput.TabIndex = 6
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(44, 555)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(199, 26)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(253, 555)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(199, 26)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 603)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtK)
+        Me.Controls.Add(Me.txtP0)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtP0 As TextBox
+    Friend WithEvents txtK As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ HassanJasim_Assignment_2.1/HassanJasim_Assignment_2.1/Form1.vb
@@ -0,0 +1,83 @@
+﻿'Hassan Jasim
+'400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblKValue, dblP0Value, dblTime As Double
+
+        If Not Double.TryParse(txtP0.Text, dblP0Value) Then
+            txtP0.Clear()
+            txtP0.Focus()
+            MsgBox("Please enter a valid P0 value",, "Error")
+            Exit Sub
+        End If
+
+        If Not Double.TryParse(txtK.Text, dblKValue) Then
+            txtK.Clear()
+            txtK.Focus()
+            MsgBox("Please enter a valid K value",, "Error")
+            Exit Sub
+        End If
+
+        Try
+            dblKValue = CDbl(txtK.Text)
+            dblP0Value = CDbl(txtP0.Text)
+            dblTime = 0
+            If dblP0Value > 0 And (dblKValue >= -0.08 And dblKValue <= 0.08) Then
+
+                If dblKValue > 0 Then
+                    DoubleTime(dblKValue, dblP0Value, dblTime)
+                    lblOutput.Text &= "P0 = " & dblP0Value & vbCr
+                    lblOutput.Text &= "K value = " & dblKValue & vbCr
+                    lblOutput.Text &= "P(t) = " & dblP0Value * 2 & vbCr
+                    dblTime = dblTime / 60
+                    lblOutput.Text &= "The time it took to get to P0 = " & dblTime.ToString("n3") & "min" & vbCr
+                End If
+
+                If dblKValue < 0 Then
+                    HalfTime(dblKValue, dblP0Value, dblTime)
+                    lblOutput.Text &= "P0 = " & dblP0Value & vbCr
+                    lblOutput.Text &= "K value = " & dblKValue & vbCr
+                    lblOutput.Text &= "P(t) = " & dblP0Value / 2 & vbCr
+                    dblTime = dblTime / 60
+                    lblOutput.Text &= "The time it took to get to P0 = " & dblTime.ToString("n3") & "min" & vbCr
+                End If
+
+            Else
+                If dblP0Value < 0 Then
+                    MsgBox("Please enter a Positive P0 value",, "Error")
+                End If
+                If dblKValue < -0.08 Or dblKValue > 0.08 Then
+                    MsgBox("Please enter a K value within the given range",, "Error")
+                End If
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error")
+        End Try
+    End Sub
+    Private Function DoubleTime(ByVal K As Double, ByVal P0 As Double, ByRef time As Double) As Double
+        lblOutput.Text = Space(35) & "This is a Growth Model" & vbCr
+        Dim dblPt As Double = 2 * P0
+
+        time = (Math.Log(dblPt / P0)) / (K)
+
+        Return time
+    End Function
+    Private Function HalfTime(ByVal K As Double, ByVal P0 As Double, ByRef time As Double) As Double
+        lblOutput.Text = Space(35) & "This is a Decay Model" & vbCr
+        Dim dblPt As Double = P0 / 2
+
+        time = (Math.Log(dblPt / P0)) / (K)
+
+        Return time
+    End Function
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtK.Clear()
+        txtP0.Clear()
+        lblOutput.Text = String.Empty
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
