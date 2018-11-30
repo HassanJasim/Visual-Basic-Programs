f57863b73c00ddd74dd01b278ccc5da5a913d0fa
Hassan Jasim <hassanjasim1000@gmail.com>
2018-11-30 05:16:56 -0500

    Add files via upload

---
+++ HassanJasim_Assignment_2.2/HassanJasim_Assignment_2.2/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDiameter = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtDepth = New System.Windows.Forms.TextBox()
+        Me.txtThickness = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(209, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Diameter of the conical tank (m)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(193, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Height of the conical tank (m)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(199, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Depth of the oil in the tank (m)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(201, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Thickness of the oil sheets (m)"
+        '
+        'txtDiameter
+        '
+        Me.txtDiameter.Location = New System.Drawing.Point(267, 27)
+        Me.txtDiameter.Name = "txtDiameter"
+        Me.txtDiameter.Size = New System.Drawing.Size(248, 22)
+        Me.txtDiameter.TabIndex = 4
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(267, 78)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(248, 22)
+        Me.txtHeight.TabIndex = 5
+        '
+        'txtDepth
+        '
+        Me.txtDepth.Location = New System.Drawing.Point(267, 129)
+        Me.txtDepth.Name = "txtDepth"
+        Me.txtDepth.Size = New System.Drawing.Size(248, 22)
+        Me.txtDepth.TabIndex = 6
+        '
+        'txtThickness
+        '
+        Me.txtThickness.Location = New System.Drawing.Point(267, 180)
+        Me.txtThickness.Name = "txtThickness"
+        Me.txtThickness.Size = New System.Drawing.Size(248, 22)
+        Me.txtThickness.TabIndex = 7
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(38, 231)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(477, 23)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "Calculate Work Done"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(38, 261)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(477, 359)
+        Me.lblOutput.TabIndex = 9
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(38, 624)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(233, 23)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(282, 624)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(233, 23)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(538, 667)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtThickness)
+        Me.Controls.Add(Me.txtDepth)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtDiameter)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDiameter As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtDepth As TextBox
+    Friend WithEvents txtThickness As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ HassanJasim_Assignment_2.2/HassanJasim_Assignment_2.2/Form1.vb
@@ -0,0 +1,84 @@
+﻿'Hassan Jasim
+'400057926
+Option Strict On
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblWidth, dblDeltaH, dblHeight, dblDiameter, dblDepth, dblWorkDone, dblTotalWork As Double
+
+        If Not Double.TryParse(txtDiameter.Text, dblDiameter) Then
+            txtDiameter.Clear()
+            txtDiameter.Focus()
+            MsgBox("Please enter a valid Diameter",, "Error")
+            Exit Sub
+        End If
+        If Not Double.TryParse(txtHeight.Text, dblHeight) Then
+            txtHeight.Clear()
+            txtHeight.Focus()
+            MsgBox("Please enter a valid Height",, "Error")
+            Exit Sub
+        End If
+        If Not Double.TryParse(txtDepth.Text, dblDepth) Then
+            txtDepth.Clear()
+            txtDepth.Focus()
+            MsgBox("Please enter a valid Depth",, "Error")
+            Exit Sub
+        End If
+        If Not Double.TryParse(txtThickness.Text, dblDeltaH) Then
+            txtThickness.Clear()
+            txtThickness.Focus()
+            MsgBox("Please enter a valid Thickness",, "Error")
+            Exit Sub
+        End If
+
+        Try
+            dblDeltaH = CDbl(txtThickness.Text)
+            dblHeight = CDbl(txtHeight.Text)
+            dblDiameter = CDbl(txtDiameter.Text)
+            dblDepth = CDbl(txtDepth.Text)
+            dblWorkDone = 0
+
+            For i = 1 To (dblDepth / dblDeltaH) Step dblDeltaH
+                Work(dblDeltaH, dblHeight, dblDiameter, dblDepth, dblWidth, dblWorkDone)
+                dblTotalWork += dblWorkDone
+            Next
+            lblOutput.Text &= "Tank Height = " & dblHeight & " m" & vbCr
+            lblOutput.Text &= "Tank Diameter = " & dblDiameter & " m" & vbCr
+            lblOutput.Text &= "Oil Height = " & dblDepth & " m" & vbCr
+            lblOutput.Text &= "Delta H = " & dblDeltaH & " m" & vbCr
+            lblOutput.Text &= "Work needed to remove the oil is = " & dblTotalWork.ToString("n3") & " J" & vbCr
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "ERROR")
+        End Try
+    End Sub
+    Private Function Volume(ByVal DeltaH As Double, ByVal tankHeight As Double, ByVal tankDiameter As Double, ByVal oilDepth As Double, ByVal oilWidth As Double) As Double
+        oilWidth = (tankDiameter * oilDepth) / tankHeight
+
+        Return Math.PI * (oilWidth / 2) ^ 2 * DeltaH
+    End Function
+    Private Function Force(ByVal DeltaH As Double, ByVal tankHeight As Double, ByVal tankDiameter As Double, ByVal oilDepth As Double, ByVal oilWidth As Double) As Double
+        Const row As Double = 2000
+        Const earthG As Double = 9.81
+        Dim V As Double
+
+        V = Volume(DeltaH, tankHeight, tankDiameter, oilDepth, oilWidth)
+
+        Return V * row * earthG
+    End Function
+    Private Function Work(ByVal DeltaH As Double, ByVal tankHeight As Double, ByVal tankDiameter As Double, ByVal oilDepth As Double, ByVal oilWidth As Double, ByRef workDone As Double) As Double
+        Dim F As Double
+
+        F = Force(DeltaH, tankHeight, tankDiameter, oilDepth, oilWidth)
+        workDone = F * (tankHeight - oilDepth)
+        Return workDone
+    End Function
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDepth.Clear()
+        txtDiameter.Clear()
+        txtHeight.Clear()
+        txtThickness.Clear()
+        lblOutput.Text = String.Empty
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
