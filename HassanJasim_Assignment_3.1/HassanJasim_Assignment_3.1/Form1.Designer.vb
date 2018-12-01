8b119946ecddd56d050e8b833b7a71f6c665654f
Hassan Jasim <hassanjasim1000@gmail.com>
2018-12-01 15:29:10 -0500

    Add files via upload

---
+++ HassanJasim_Assignment_3.1/HassanJasim_Assignment_3.1/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Options = New System.Windows.Forms.GroupBox()
+        Me.radFile = New System.Windows.Forms.RadioButton()
+        Me.rad2D = New System.Windows.Forms.RadioButton()
+        Me.radInput = New System.Windows.Forms.RadioButton()
+        Me.rad1D = New System.Windows.Forms.RadioButton()
+        Me.btnEstimate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.Options.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Options
+        '
+        Me.Options.Controls.Add(Me.radFile)
+        Me.Options.Controls.Add(Me.rad2D)
+        Me.Options.Controls.Add(Me.radInput)
+        Me.Options.Controls.Add(Me.rad1D)
+        Me.Options.Location = New System.Drawing.Point(12, 12)
+        Me.Options.Name = "Options"
+        Me.Options.Size = New System.Drawing.Size(234, 92)
+        Me.Options.TabIndex = 0
+        Me.Options.TabStop = False
+        Me.Options.Text = "Options"
+        '
+        'radFile
+        '
+        Me.radFile.AutoSize = True
+        Me.radFile.Location = New System.Drawing.Point(137, 50)
+        Me.radFile.Name = "radFile"
+        Me.radFile.Size = New System.Drawing.Size(86, 21)
+        Me.radFile.TabIndex = 3
+        Me.radFile.TabStop = True
+        Me.radFile.Text = "File Input"
+        Me.radFile.UseVisualStyleBackColor = True
+        '
+        'rad2D
+        '
+        Me.rad2D.AutoSize = True
+        Me.rad2D.Location = New System.Drawing.Point(137, 22)
+        Me.rad2D.Name = "rad2D"
+        Me.rad2D.Size = New System.Drawing.Size(85, 21)
+        Me.rad2D.TabIndex = 2
+        Me.rad2D.TabStop = True
+        Me.rad2D.Text = "2D Array"
+        Me.rad2D.UseVisualStyleBackColor = True
+        '
+        'radInput
+        '
+        Me.radInput.AutoSize = True
+        Me.radInput.Location = New System.Drawing.Point(16, 50)
+        Me.radInput.Name = "radInput"
+        Me.radInput.Size = New System.Drawing.Size(107, 21)
+        Me.radInput.TabIndex = 1
+        Me.radInput.TabStop = True
+        Me.radInput.Text = "Input Values"
+        Me.radInput.UseVisualStyleBackColor = True
+        '
+        'rad1D
+        '
+        Me.rad1D.AutoSize = True
+        Me.rad1D.Location = New System.Drawing.Point(16, 22)
+        Me.rad1D.Name = "rad1D"
+        Me.rad1D.Size = New System.Drawing.Size(85, 21)
+        Me.rad1D.TabIndex = 0
+        Me.rad1D.TabStop = True
+        Me.rad1D.Text = "1D Array"
+        Me.rad1D.UseVisualStyleBackColor = True
+        '
+        'btnEstimate
+        '
+        Me.btnEstimate.Location = New System.Drawing.Point(252, 19)
+        Me.btnEstimate.Name = "btnEstimate"
+        Me.btnEstimate.Size = New System.Drawing.Size(75, 85)
+        Me.btnEstimate.TabIndex = 1
+        Me.btnEstimate.Text = "Estimate"
+        Me.btnEstimate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(333, 62)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 42)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(333, 19)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 37)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDisplay.Location = New System.Drawing.Point(13, 111)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(395, 217)
+        Me.lblDisplay.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(418, 340)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEstimate)
+        Me.Controls.Add(Me.Options)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Options.ResumeLayout(False)
+        Me.Options.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Options As GroupBox
+    Friend WithEvents radFile As RadioButton
+    Friend WithEvents rad2D As RadioButton
+    Friend WithEvents radInput As RadioButton
+    Friend WithEvents rad1D As RadioButton
+    Friend WithEvents btnEstimate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblDisplay As Label
+End Class
---
+++ HassanJasim_Assignment_3.1/HassanJasim_Assignment_3.1/Form1.vb
@@ -0,0 +1,145 @@
+﻿'Hassan Jasim
+'400057926
+Option Strict On
+Imports System.IO
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim outFile As StreamWriter
+        outFile = File.CreateText("Data.txt")
+        outFile.WriteLine("10,5")
+        outFile.WriteLine("15,20")
+        outFile.WriteLine("20,18")
+        outFile.WriteLine("25,40")
+        outFile.WriteLine("40,33")
+        outFile.WriteLine("50,54")
+        outFile.WriteLine("55,70")
+        outFile.WriteLine("60,60")
+        outFile.WriteLine("75,78")
+        outFile.Close()
+    End Sub
+
+    Private Sub btnEstimate_Click(sender As Object, e As EventArgs) Handles btnEstimate.Click
+        Dim inputFile As StreamReader
+        Dim timeArray() As Double = {10, 15, 20, 25, 40, 50, 55, 60, 75}
+        Dim tStrengthArray() As Double = {5, 20, 18, 40, 33, 54, 70, 60, 78}
+        Dim SofT(,) As Double = {{10, 15, 20, 25, 40, 50, 55, 60, 75},
+                                  {5, 20, 18, 40, 33, 54, 70, 60, 78}}
+        Dim fileTimeArray() As Double
+        Dim fileTesileArray() As Double
+        Dim inputTimeArray() As Double
+        Dim inputTensileArray() As Double
+        Dim tempArray(1) As String
+        Dim arrayLength As Integer
+        Dim dblSlope, dblYint, dblTime, dblTstrength As Double
+
+        If rad1D.Checked Then
+            Tensile_1D(timeArray, tStrengthArray, dblSlope, dblYint)
+            If getTime(dblTime) Then
+                dblTstrength = dblTime * dblSlope + dblYint
+            End If
+            printValues(dblSlope, dblYint, dblTime, dblTstrength)
+        ElseIf rad2D.Checked Then
+            Tensile_2D(SofT, dblSlope, dblYint)
+            If getTime(dblTime) Then
+                dblTstrength = dblTime * dblSlope + dblYint
+            End If
+            printValues(dblSlope, dblYint, dblTime, dblTstrength)
+        ElseIf radInput.Checked Then
+            arrayLength = CInt(InputBox("Please enter the number of data collected", "Array Length"))
+            ReDim inputTimeArray(arrayLength - 1)
+            ReDim inputTensileArray(arrayLength - 1)
+            inputTensileArray = getInputArray(inputTimeArray, inputTensileArray, arrayLength)
+            Tensile_1D(inputTimeArray, inputTensileArray, dblSlope, dblYint)
+            If getTime(dblTime) Then
+                dblTstrength = dblTime * dblSlope + dblYint
+            End If
+            printValues(dblSlope, dblYint, dblTime, dblTstrength)
+        ElseIf radFile.Checked Then
+            If File.Exists("Data.txt") Then
+                inputFile = File.OpenText("Data.txt")
+                arrayLength = 0
+                Do Until inputFile.EndOfStream
+                    inputFile.ReadLine()
+                    arrayLength += 1
+                Loop
+                inputFile.Close()
+                inputFile = File.OpenText("Data.txt")
+                ReDim fileTimeArray(arrayLength - 1)
+                ReDim fileTesileArray(arrayLength - 1)
+                Do Until inputFile.EndOfStream
+                    For i = 0 To arrayLength - 1
+                        tempArray = Split(inputFile.ReadLine(), ",")
+                        fileTimeArray(i) = CDbl(tempArray(0))
+                        fileTesileArray(i) = CDbl(tempArray(1))
+                    Next
+                Loop
+                Tensile_1D(fileTimeArray, fileTesileArray, dblSlope, dblYint)
+                If getTime(dblTime) Then
+                    dblTstrength = dblTime * dblSlope + dblYint
+                End If
+                printValues(dblSlope, dblYint, dblTime, dblTstrength)
+                inputFile.Close()
+            Else
+                MsgBox("File does not exist",, "ERROR")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblDisplay.Text = String.Empty
+        rad1D.Checked = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Tensile_1D(ByVal timeArray() As Double, ByVal tStrengthArray() As Double, ByRef dblSlope As Double, ByRef dblYint As Double)
+        Dim sum1, sum2, sum3, sum4 As Double
+        For i = 0 To timeArray.Length - 1
+            sum1 += timeArray(i) * tStrengthArray(i)
+            sum2 += timeArray(i)
+            sum3 += tStrengthArray(i)
+            sum4 += timeArray(i) ^ 2
+        Next
+        dblSlope = ((9 * sum1) - (sum2 * sum3)) / ((9 * sum4) - (sum2 ^ 2))
+        dblYint = (sum3 / 9) - ((sum2 / 9) * dblSlope)
+    End Sub
+
+    Private Sub Tensile_2D(ByVal SofT(,) As Double, ByRef dblSlope As Double, ByRef dblYint As Double)
+        Dim sum1, sum2, sum3, sum4 As Double
+        For i = 0 To 8
+            sum1 += SofT(0, i) * SofT(1, i)
+            sum2 += SofT(0, i)
+            sum3 += SofT(1, i)
+            sum4 += SofT(0, i) ^ 2
+        Next
+        dblSlope = ((9 * sum1) - (sum2 * sum3)) / ((9 * sum4) - (sum2 ^ 2))
+        dblYint = (sum3 / 9) - ((sum2 / 9) * dblSlope)
+    End Sub
+
+    Private Function getTime(ByRef dblTime As Double) As Boolean
+
+        dblTime = CDbl(InputBox("Please enter the value of time (10 - 75 inclusive)", "Input"))
+        If dblTime <= 75 And dblTime >= 10 Then
+            Return True
+        Else
+            MsgBox("Please enter a time between 10 - 75 (inclusive)")
+            Return False
+        End If
+    End Function
+
+    Private Function getInputArray(ByRef inputTimeArray() As Double, ByVal inputTensileArray() As Double, ByVal arrayLength As Integer) As Double()
+        For i = 0 To arrayLength - 1
+            inputTimeArray(i) = CInt(InputBox("Please enter the time", "Input"))
+            inputTensileArray(i) = CInt(InputBox("Please enter the tensile strength", "Input"))
+        Next
+        Return inputTensileArray
+    End Function
+
+    Private Sub printValues(ByVal dblSlope As Double, ByVal dblYint As Double, ByVal dblTime As Double, ByVal dblTstrength As Double)
+        lblDisplay.Text = "The time is: " & dblTime.ToString("n3") & vbCr & "The slope is: " & dblSlope.ToString("n3") & vbCr & "The Y-int is: " & dblYint.ToString("n3") & vbCr & "The Tensile Strength is: " & dblTstrength.ToString("n3")
+    End Sub
+
+End Class
