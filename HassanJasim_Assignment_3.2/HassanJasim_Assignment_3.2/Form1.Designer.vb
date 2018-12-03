606786a402c1004a52dd80586a8b713634287927
Hassan Jasim <hassanjasim1000@gmail.com>
2018-12-02 23:36:23 -0500

    Add files via upload

---
+++ HassanJasim_Assignment_3.2/HassanJasim_Assignment_3.2/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(12, 12)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(93, 36)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(111, 12)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(93, 36)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(210, 12)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 36)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDisplay.Location = New System.Drawing.Point(12, 51)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(274, 235)
+        Me.lblDisplay.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 299)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDisplay As Label
+End Class
---
+++ HassanJasim_Assignment_3.2/HassanJasim_Assignment_3.2/Form1.vb
@@ -0,0 +1,78 @@
+﻿'Hassan Jasim 
+'400057926
+Option Strict On
+Imports System.IO
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Const gasConstant As Double = 8.314
+        Const moles As Double = 1000
+        Dim inputFile As StreamReader
+        Dim outFile As StreamWriter
+        Dim intermString As String
+        Dim idxPressure, idxTemperature, idxVolume As Integer
+        Dim intermArray(2) As String
+        Dim dblPressure, dblVolume, dblTemperature As Double
+
+        If File.Exists("idealGas.txt") Then
+            inputFile = File.OpenText("idealGas.txt")
+
+            Do Until inputFile.EndOfStream
+                intermString = inputFile.ReadLine()
+
+                idxPressure = intermString.IndexOf("Pressure")
+                idxTemperature = intermString.IndexOf("Temperature")
+                idxVolume = intermString.IndexOf("Volume")
+
+                If idxPressure = 0 Then
+                    intermArray = Split(intermString, " ")
+                    dblPressure = CDbl(intermArray(1))
+                ElseIf idxTemperature = 0 Then
+                    intermArray = Split(intermString, " ")
+                    dblTemperature = CDbl(intermArray(1))
+                ElseIf idxVolume = 0 Then
+                    intermArray = Split(intermString, " ")
+                    dblVolume = CDbl(intermArray(1))
+                End If
+
+            Loop
+            inputFile.Close()
+
+
+            If dblPressure = 0 Then
+                dblPressure = (moles * gasConstant * dblTemperature) / dblVolume
+            ElseIf dblTemperature = 0 Then
+                dblTemperature = (dblPressure * dblVolume) / (moles * gasConstant)
+            ElseIf dblVolume = 0 Then
+                dblVolume = (moles * gasConstant * dblTemperature) / dblPressure
+            End If
+
+
+            lblDisplay.Text = "Pressure: " & dblPressure.ToString("n2") & " Pa" & vbCr
+            lblDisplay.Text &= "Temperature: " & dblTemperature.ToString("n2") & " K" & vbCr
+            lblDisplay.Text &= "Volume: " & dblVolume.ToString("n2") & " m^3" & vbCr
+            lblDisplay.Text &= "Gas Constant: " & gasConstant & " J/(k*mol)" & vbCr
+            lblDisplay.Text &= "Moles: " & moles & " moles" & vbCr
+
+            outFile = File.CreateText("idealGas.txt")
+
+            outFile.WriteLine("Pressure: " & dblPressure.ToString("n2") & " Pa")
+            outFile.WriteLine("Temperature: " & dblTemperature.ToString("n2") & " K")
+            outFile.WriteLine("Volume: " & dblVolume.ToString("n2") & " m^3")
+            outFile.WriteLine("Gas Constant: " & gasConstant & " J/(k*mol)")
+            outFile.WriteLine("Moles: " & moles & " moles")
+
+            outFile.Close()
+        Else
+            MsgBox("File does not exist",, "ERROR")
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblDisplay.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
